6acdb1a9a06d7c745314a9c12adb7ca1051a08e8
Onufriieva <onufrieva.julia@gmail.com>
2022-11-07 14:59:35 +0200

    *

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { nanoid } from 'nanoid';
 import { login } from 'redux/operations';
 // import { getIsLoged } from '../../redux/selectors';
